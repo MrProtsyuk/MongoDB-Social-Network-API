8b5d880855525b7fbfb3e0618fcb077ebe5ce666
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-05-01 21:39:09 -0700

    finished user model'

--- models/User.js
+++ models/User.js
@@ -13,39 +13,32 @@ const UserSchema = new Schema({
         unique: true,
         match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
     },
-    thoughts: [],
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
     friends: [
         {
             type: Schema.Types.ObjectId,
-            ref: 'Frineds'
+            ref: 'User'
         }
     ]
-})
-
-// * `username`
-//     * String
-//     * Unique
-//     * Required
-//     * Trimmed
-
-// * `email`
-//     * String
-//     * Required
-//     * Unique
-//     * Must match a valid email address (look into Mongoose's matching validation)
-
-// * `thoughts`
-//     * Array of `_id` values referencing the `Thought` model
-
-// * `friends`
-//     * Array of `_id` values referencing the `User` model (self-reference)
-
-// **Schema Settings**
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
+);
 
 // Create a virtual called `friendCount` that retrieves the length of the user's `friends` array field on query
 UserSchema.virtual('friendCount').get(function() {
-    return this.friends.reduce((total, friends) => total + friends.replies.length + 1, 0);
-  });
+    return this.friends.length; 
+})
 
 const User = model('User', UserSchema);
 
